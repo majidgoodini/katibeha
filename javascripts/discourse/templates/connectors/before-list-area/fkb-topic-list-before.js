aa223b100fdf8d67fa97da4063d23fe108c3f7bb
majidgoodini <majid.goodini@gmail.com>
2024-08-29 19:54:44 +0330

    ✨: add best teachers

--- javascripts/discourse/templates/connectors/before-list-area/fkb-topic-list-before.js
+++ javascripts/discourse/templates/connectors/before-list-area/fkb-topic-list-before.js
@@ -1,8 +1,23 @@
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
 import { withPluginApi } from "discourse/lib/plugin-api";
+import { ajax } from "discourse/lib/ajax";
 
-export default {
-  setupComponent(attrs, component) {
+export default class BeforeTopicList extends Component {
+  @tracked items = [];
+
+  constructor() {
+    super(...arguments);
+    this.loadItems();
+  }
+
+  loadItems() {
     withPluginApi("0.8.7", (api) => {
-     });
-  },
-};
+      ajax("https://app.nocodb.com/api/v1/db/public/shared-view/c64f2002-b5dc-48ce-8912-e8f2bfc04713/rows").then((result) => {
+        this.items = result.list;
+      }).catch((error) => {
+        console.error("Failed to fetch items:", error);
+      });
+    });
+  }
+}
